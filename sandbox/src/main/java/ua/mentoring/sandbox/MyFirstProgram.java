1edb1428e9826407322665187b6288049bd836f6
ysherenkova <yulia.sherenkova@gmail.com>
2020-01-18 00:16:54 +0200

    lesson 14

--- sandbox/src/main/java/ua/mentoring/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/mentoring/sandbox/MyFirstProgram.java
@@ -8,12 +8,15 @@ public class MyFirstProgram {
         hello("user");
         hello("jopa");
 
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        Square square = new Square(5);
+ //       square.l = 5.0;
+        System.out.println("Площадь квадрата со стороной " + square.l + " = " + area(square));
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами" + a + " и " + b + "=" + area(a,b));
+        Rectangle rectangle = new Rectangle(4,6);
+ //       rectangle.a = 4;
+ //       rectangle.b = 6;
+
+        System.out.println("Площадь прямоугольника со сторонами " + rectangle.a + " и " + rectangle.b + " = " + area(rectangle));
 
     }
 
@@ -21,12 +24,12 @@ public class MyFirstProgram {
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area (double len){ //помни про область видимости
-        return len * len;
+    public static double area (Square square){ //помни про область видимости
+        return square.l * square.l;
     }
 
-    public static double area (double a, double b){ //переопределение функции
-        return a * b;
+    public static double area (Rectangle rectangle){ //переопределение функции
+        return rectangle.a * rectangle.b;
     }
 
 }
---
+++ sandbox/src/main/java/ua/mentoring/sandbox/Square.java
@@ -0,0 +1,10 @@
+package ua.mentoring.sandbox;
+
+public class Square {
+
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+}
