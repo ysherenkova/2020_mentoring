c087e78b4b0a5eaadc67df1bf8de54753a8f05de
ysherenkova <yulia.sherenkova@gmail.com>
2020-01-17 23:38:10 +0200

    lesson 10

--- sandbox/src/main/java/ua/mentoring/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/mentoring/sandbox/MyFirstProgram.java
@@ -3,7 +3,11 @@ package ua.mentoring.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, " + "world!");
+
+        int a = 5;
+        int s = a * a;
+        System.out.println("Площадь квадрата со стороной " + a + " = " + s);
     }
 
 }
