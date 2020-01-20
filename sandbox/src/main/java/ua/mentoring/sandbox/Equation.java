1b69048a6e7be9118b5e7def2b7c45962b8d8d15
ysherenkova <yulia.sherenkova@gmail.com>
2020-01-20 12:39:47 +0200

    lesson 28: if-else

---
+++ sandbox/src/main/java/ua/mentoring/sandbox/Equation.java
@@ -0,0 +1,32 @@
+package ua.mentoring.sandbox;
+
+public class Equation {
+
+
+  private double a;
+  private double b;
+  private double c;
+
+  private int number;
+
+  public Equation(double a, double b, double c) {
+
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    double d = b * b - 4 * a * c;
+    if (d > 0)
+      number = 2;
+    else {
+      if (d == 0)
+        number = 1;
+      else
+        number = 0;
+    }
+  }
+
+  public int rootNumber() {
+    return number;
+}
+}
