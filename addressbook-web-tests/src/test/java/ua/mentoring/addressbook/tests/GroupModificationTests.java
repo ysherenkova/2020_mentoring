5352380bd1521f115be71217e71b13f6e8d86d95
ysherenkova <yulia.sherenkova@gmail.com>
2020-01-20 10:37:46 +0200

    lesson 27: added new tet - GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/ua/mentoring/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ua.mentoring.addressbook.tests;
+
+import org.junit.Test;
+import ua.mentoring.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupNotification () {
+    applicationManager.getNavigationHelper().gotoGroupPage();
+    applicationManager.getGroupHelper().selectGroup();
+    applicationManager.getGroupHelper().initGroupModification();
+    applicationManager.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    applicationManager.getGroupHelper().submitGroupModification();
+    applicationManager.getGroupHelper().returnToGroupPage();
+  }
+}
