cd5c0ce90e3f46a04d0cc7bbcf0436e20f3ed7d7
ysherenkova <yulia.sherenkova@gmail.com>
2020-03-02 22:15:04 +0200

    lesson 48: how to run browser once

--- addressbook-web-tests/src/test/java/ua/mentoring/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ua/mentoring/addressbook/tests/TestBase.java
@@ -1,20 +1,22 @@
 package ua.mentoring.addressbook.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import ua.mentoring.addressbook.appManager.ApplicationManager;
 import ua.mentoring.addressbook.model.BrowserType;
 
 public class TestBase {
 
-  protected final ApplicationManager applicationManager = new ApplicationManager(BrowserType.FIREFOX); //here we can change the browser
+  protected static final ApplicationManager applicationManager = new ApplicationManager(BrowserType.FIREFOX); //here we can change the browser
 
-  @BeforeMethod
+ // @BeforeMethod
+  @BeforeSuite
   public void setUp() {
     applicationManager.init();
   }
 
-  @AfterMethod
+ // @AfterMethod
+  @AfterSuite
   public void tearDown() {
     applicationManager.stop();
   }
