3acdb3abedd74dcd6785951416e48ec78b1c16e3
ysherenkova <yulia.sherenkova@gmail.com>
2020-03-10 19:22:15 +0200

    lesson 65: added json test data generator

--- addressbook-web-tests/src/test/java/ua/mentoring/addressbook/Generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ua/mentoring/addressbook/Generators/GroupDataGenerator.java
@@ -3,6 +3,8 @@ package ua.mentoring.addressbook.Generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.internal.bind.util.ISO8601Utils;
 import com.thoughtworks.xstream.XStream;
 import ua.mentoring.addressbook.model.GroupData;
@@ -45,8 +47,18 @@ public class GroupDataGenerator {
       saveAsCsv(groups, new File(file));
     else if (format.equals("xml"))
       saveAsXml(groups, new File(file));
+    else if (format.equals("json"))
+      saveAsJson(groups, new File(file));
     else
       System.out.println("Unrecognized format" + format);
+  }
+
+  private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
   }
 
   private void saveAsXml(List<GroupData> groups, File file) throws IOException {
