c0ff1ff90e46837fefe55a564404edde43cf19c3
ysherenkova <yulia.sherenkova@gmail.com>
2020-03-10 18:13:46 +0200

    lesson 63: now I can take test data from a file

--- addressbook-web-tests/src/test/java/ua/mentoring/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/mentoring/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ua.mentoring.addressbook.model.GroupData;
 import ua.mentoring.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,18 +16,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() {
+  public Iterator<Object[]> validGroups() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {"test1", "header 1", "footer 1"});
-    list.add(new Object[] {"test2", "header 2", "footer 2"});
-    list.add(new Object[] {"test3", "header 3", "footer 3"});
-
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null)
+    {
+      String[] split = line.split(";");
+      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
   @Test(dataProvider = "validGroups")
-  public void testGroupCreation(String name, String header, String footer) {
-    GroupData group = new GroupData().withName(name).withHeader(header).withFooter(footer);
+  public void testGroupCreation(GroupData group) {
     applicationManager.goTo().groupPage();
     Groups before = applicationManager.group().all();
     applicationManager.group().create(group);
